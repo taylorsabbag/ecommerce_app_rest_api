1f8a5f65d154d0489095d225f33cdb72cda4f501
taylorsabbag <38229747+taylorsabbag@users.noreply.github.com>
2022-06-22 12:09:32 -0400

    Simplified and removed client object.

--- db/index.js
+++ db/index.js
@@ -8,24 +8,8 @@ const pool = new Pool({
   port: "5432",
 });
 
-pool.query("SELECT NOW()", (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
-
-const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "ecommerce",
-  password: "postgres",
-  port: "5432",
-});
-await client.connect()
-const res = await client.query('SELECT NOW()')
-await client.end()
-
 module.exports = {
   query: (text, params, callback) => {
-    return pool.query(text, params, callback);
+    return pool.query(text, params, callback)
   },
-};
+}
